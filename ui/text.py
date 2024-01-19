7fcd0eee00444283065ae8cfb8e5eef963cbcc8c
YoAquinJS <sebascertuche@gmail.com>
2024-01-19 17:52:42 -0500

    Added dataclass to Text

--- ui/text.py
+++ ui/text.py
@@ -1,9 +1,9 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 import pygame
 
 @dataclass
 class Text():
     value: str
     scale: float
-    characterImages: list[pygame.Surface] = []
-    renderPositions: list[tuple[int, int]] = []
+    characterImages: list[pygame.Surface] = field(default_factory=list[pygame.Surface])
+    renderPositions: list[tuple[int, int]] = field(default_factory=list[tuple[int, int]])
