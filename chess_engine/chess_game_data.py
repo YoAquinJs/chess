b7d67b931cae55e7296cd84bd8f30125697c3d10
YoAquinJS <sebascertuche@gmail.com>
2024-01-26 17:39:16 -0500

    addded ChessGameData class

---
+++ chess_engine/chess_game_data.py
@@ -0,0 +1,88 @@
+"""TODO"""
+
+from __future__ import annotations
+
+from dataclasses import dataclass
+from enum import Enum, auto
+from typing import Any, Optional
+
+from chess_engine.piece import Piece, SerPiece
+from chess_engine.structs import Coord
+from game_logic.consts import PlayerColor
+from serialization.serializable import Serializable
+from utils.parseable_enum import ParseableEnum
+
+
+class GameStatus(Enum, metaclass=ParseableEnum):
+    """Enum for states in a chess game
+    """
+    PENDING = auto()
+    WHITE_WIN = auto()
+    BLACK_WIN = auto()
+    STALEMATE = auto()
+    TIE = auto()
+
+MoveHistory = list[tuple[Piece, Piece | Coord]]
+SerMoveHistory = list[tuple[SerPiece, SerPiece | tuple[int,int]]]
+@dataclass
+class ChessGameData(Serializable):
+    """TODO
+    """
+
+    status: GameStatus
+    turn: PlayerColor
+    white_castle_left: bool
+    white_castle_right: bool
+    black_castle_left: bool
+    black_castle_right: bool
+    move_history: MoveHistory
+
+    def get_serialization_attrs(self) -> dict[str, Any]:
+        def serializable_move_history(move_history: MoveHistory) -> SerMoveHistory:
+            ser_move_history: SerMoveHistory = []
+            for piece, dest in move_history:
+                ser_dest = (dest.row, dest.column) if isinstance(dest, Coord) else dest.serialize()
+                ser_move_history.append((piece.serialize(), ser_dest))
+            return ser_move_history
+
+        return {
+            "gameStatus"         : self.status.value,
+            "turn"               : self.turn.value,
+            "white_castle_left"  : self.white_castle_left,
+            "white_castle_right" : self.white_castle_right,
+            "black_castle_left"  : self.black_castle_left,
+            "black_castle_right" :self.black_castle_right,
+            "moveHistory" : serializable_move_history(self.move_history)
+        }
+
+    @classmethod
+    def get_from_deserialize(cls, attrs: dict[str, Any], **kwargs: Any) -> ChessGameData:
+        """TODO
+        """
+        def _parse_move_history(ser_move_history: SerMoveHistory) -> MoveHistory:
+            def get_dest(ser_dest: SerPiece | tuple[int,int]) -> Optional[Piece] | Coord:
+                match ser_dest:
+                    case (_, (_, _)):
+                        return Piece.deserialize(ser_dest)
+                    case (_, _):
+                        return Coord(*ser_dest)
+
+            move_history: MoveHistory = []
+            for ser_piece, ser_dest in ser_move_history:
+                piece = Piece.deserialize(ser_piece)
+                dest = get_dest(ser_dest)
+                if piece is None or dest is None:
+                    raise ValueError(f"Invalid piece {ser_piece} or destination \
+                                     {ser_dest} in GameData deserialization")
+                move_history.append((piece, dest))
+            return move_history
+
+        return ChessGameData(
+            attrs["gameStatus"],
+            attrs["turn"],
+            attrs["white_castle_left"],
+            attrs["white_castle_right"],
+            attrs["black_castle_left"],
+            attrs["black_castle_right"],
+            _parse_move_history(attrs["moveHistory"])
+            )
