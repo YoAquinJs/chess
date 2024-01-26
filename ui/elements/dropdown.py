a3465bf09083e933feb28f70edbc6a6f2be78029
YoAquinJS <sebascertuche@gmail.com>
2024-01-26 17:48:39 -0500

    Formatting

--- ui/elements/dropdown.py
+++ ui/elements/dropdown.py
@@ -1,31 +1,31 @@
+from dataclasses import dataclass
+
 import pygame
 
-from typing import Union
-from ui.game_object import GameObject
 from ui.elements.button import Button
 from ui.elements.label import Label
 from ui.elements.labeled_sprite import LabeledSprite
 from ui.elements.scroll import Scroll
+from ui.game_object import GameObject
+
+
+@dataclass
+class DropDownInitData():
+    """TODO
+    """
+    header: LabeledSprite | Label | Button
+    body_rect: pygame.Rect
+    scroll: Scroll
+    option: GameObject
 
 class DropDown(GameObject):
-    
-    def __init__(self, x: int, y: int, header: Union[LabeledSprite, Label, Button], bodyRect: pygame.Rect, scroll: Scroll, option: GameObject) -> None:
+    """TODO
+    """
+
+    def __init__(self, x: int, y: int, data: DropDownInitData) -> None:
         super().__init__()
-        
-        self.contractible = isinstance(header, Button)
+        self.drag_children = True
+
+        self.contractible = isinstance(data.header, Button)
         self.x = x
         self.y = y
-
-    @GameObject.x.setter
-    def x(self, value):
-        delta = value - self._x
-        super().x.__set__(self, value)
-        for child in self.children:
-            child.x += delta
-
-    @GameObject.y.setter
-    def y(self, value):
-        delta = value - self._y
-        super().y.__set__(self, value)
-        for child in self.children:
-            child.y += delta
