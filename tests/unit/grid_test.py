b291f1dadd549f54b7a38c16a4a99749689dba90
YoAquinJS <sebascertuche@gmail.com>
2024-02-12 10:42:04 -0600

    Grid boundaries test

--- tests/unit/grid_test.py
+++ tests/unit/grid_test.py
@@ -3,11 +3,12 @@
 from typing import Optional
 
 import pytest
-from chess_engine.enums import PieceType, SideColor
 
+from chess_engine.enums import PieceType, SideColor
 from chess_engine.grid import BOARD_START, Grid
 from chess_engine.piece import Piece
 from chess_engine.structs import Coord
+from utils.exceptions import InvalidGridError
 
 
 def test_grid_initialization() -> None:
@@ -38,10 +39,77 @@ def test_grid_initialization() -> None:
     Grid.from_str_grid(str_list_grid)
     Grid.get_start_grid()
 
-def test_grid_boundaries() -> None:
+@pytest.mark.parametrize("grid, valid", [
+    (BOARD_START,
+     True),
+    ([['bR', 'bK', 'bB', 'bQ', 'b@', 'bB', '  '],
+      ['bP', '  ', '  ', 'bP', 'bP', 'bP', 'bP', 'bP'],
+      ['  ', 'bP', '  ', '  ', '  ', 'bK', '  ', '  '],
+      ['wR', '  ', '  ', '  ', '  ', '  ', '  ', '  '],
+      ['  ', 'wP', 'bP', '  ', 'w@', '  ', '  ', '  '],
+      ['  ', '  ', '  ', '  ', '  ', '  ', '  ', 'bR'],
+      ['wP', '  ', 'wP', 'wP', 'wP', 'wP', 'wP'],
+      ['  ', 'wK', 'wB', 'wQ', '  ', 'wB', 'wK', 'wR']],
+      False),
+    ([['bR', 'bK', 'bB', 'bQ', 'b@', 'bB', 'bK', 'bR', '  '],
+      ['wP', 'wP', 'wP', 'wP', 'wP', 'wP', 'wP', 'wP'],
+      ['  ', '  ', '  ', '  ', '  ', '  ', '  ', '  '],
+      ['  ', '  ', '  ', '  ', '  ', '  ', '  ', '  ', '  '],
+      ['  ', '  ', '  ', '  ', '  ', '  ', '  ', '  '],
+      ['  ', '  ', '  ', '  ', '  ', '  ', '  ', '  '],
+      ['bP', 'bP', 'bP', 'bP', 'bP', 'bP', 'bP', 'bP'],
+      ['wR', 'wK', 'wB', 'wQ', 'w@', 'wB', 'wK', 'wR']],
+      False),
+    ([['wP', 'wP', 'wP', 'wP', 'wP', 'wP', 'wP', 'wP'],
+      ['wR', 'wK', 'wB', 'wQ', 'w@', 'wB', 'wK', 'wR'],
+      ['  ', '  ', '  ', '  ', '  ', '  ', '  ', '  '],
+      ['  ', '  ', '  ', '  ', '  ', '  ', '  ', '  '],
+      ['  ', '  ', '  ', '  ', '  ', '  ', '  ', '  '],
+      ['  ', '  ', '  ', '  ', '  ', '  ', '  ', '  '],
+      ['bR', 'bK', 'bB', 'bQ', 'b@', 'bB', 'bK', 'bR'],
+      ['bR', 'bK', 'bB', 'bQ', 'b@', 'bB', 'bK', 'bR'],
+      ['bP', 'bP', 'bP', 'bP', 'bP', 'bP', 'bP', 'bP']],
+      False),
+    ([['bR', 'bK', 'bB', 'bQ', 'b@', 'bB', 'bK', 'bR'],
+      ['bP', 'bP', 'bP', 'bP', 'bP', 'bP', 'bP', 'bP'],
+      ['  ', '  ', '  ', '  ', '  ', '  ', '  ', '  '],
+      ['  ', '  ', '  ', '  ', '  ', '  ', '  ', '  '],
+      ['wP', 'wP', 'wP', 'wP', 'wP', 'wP', 'wP', 'wP'],
+      ['wR', 'wK', 'wB', 'wQ', 'w@', 'wB', 'wK', 'wR']],
+      False),
+])
+def test_grid_boundaries(grid: list[list[str]], valid: bool) -> None:
     """TODO
     """
-    pass
+    _valid = True
+    try:
+        Grid.from_str_grid(grid)
+    except InvalidGridError:
+        _valid = False
+    finally:
+        assert _valid == valid
+
+@pytest.mark.parametrize("grid, valid", [
+    (Coord(0,0), True),
+    (Coord(7,7), True),
+    (Coord(-1,0), False),
+    (Coord(8,0), False),
+    (Coord(0,-1), False),
+    (Coord(0,8), False),
+    (Coord(-1,-1), False),
+    (Coord(8,8), False),
+])
+def test_grid_get_boundaries(coord: Coord, valid: bool) -> None:
+    """TODO
+    """
+    grid = Grid.get_start_grid()
+    _valid = True
+    try:
+        grid.get_at(coord)
+    except IndexError:
+        _valid = False
+    finally:
+        assert _valid == valid
 
 def test_grid_get(grid: Grid, coord: Coord, expected: Optional[Piece]) -> None:
     """TODO
@@ -58,7 +126,7 @@ def test_grid_swap(grid: Grid, coord1: Coord, coord2: Coord) -> None:
     """
     pass
 
-@pytest.mark.parametrize("grid1, grid2, expected", [
+@pytest.mark.parametrize("grid1, grid2, equal", [
     (BOARD_START,
      BOARD_START,
      True),
@@ -148,10 +216,10 @@ def test_grid_swap(grid: Grid, coord1: Coord, coord2: Coord) -> None:
       ['bR', 'bK', 'bB', 'bQ', 'b@', 'bB', 'bK', 'bR']],
      False),
 ])
-def test_grid_equality(grid1: list[list[str]], grid2: list[list[str]], expected: bool) -> None:
+def test_grid_equality(grid1: list[list[str]], grid2: list[list[str]], equal: bool) -> None:
     """TODO
     """
-    assert (Grid.from_str_grid(grid1) == Grid.from_str_grid(grid2)) == expected
+    assert (Grid.from_str_grid(grid1) == Grid.from_str_grid(grid2)) == equal
 
 def test_grid_iterator(grid: Grid) -> None:
     """TODO
