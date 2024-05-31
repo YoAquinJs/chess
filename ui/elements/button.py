ee98fc286a42595155250b7698133724f034417d
YoAquinJS <sebascertuche@gmail.com>
2024-05-31 17:58:42 -0600

    Refactor button logic

--- ui/elements/button.py
+++ ui/elements/button.py
@@ -1,14 +1,17 @@
 """This module contains the button object, which is the UI element for button interactions"""
 
+from __future__ import annotations
+
 from collections.abc import Callable
 from dataclasses import dataclass
+from enum import Enum, auto
 from typing import Optional
 
 import pygame
 
-from ui.game_object import GameObject
 from ui.elements.label import Label
 from ui.elements.sprite import Sprite
+from ui.game_object import GameObject
 
 
 @dataclass
@@ -22,13 +25,21 @@ class ButtonInitData():
 class Button(GameObject):
     """TODO
     """
+
+    class State(Enum):
+        """TODO
+        """
+        IDLE = auto()
+        HOOVER = auto()
+        PRESSED = auto()
+        RELEASED = auto()
+
     def __init__(self, x: int, y: int, init_data: ButtonInitData) -> None:
         super().__init__()
         self.drag_children = True
 
         self._x = x
         self._y = y
-        self.pressed = False
         self.callback = init_data.callback
 
         self.sprite = init_data.sprite
@@ -41,30 +52,55 @@ class Button(GameObject):
             init_data.label.y = self.y
             self.add_child(1, init_data.label)
 
+        self._state = self.State.IDLE
+
+    @property
+    def _state(self) -> Button.State:
+        """TODO
+        """
+        return self.state
+
+    @_state.setter
+    def _state(self, value: Button.State) -> None:
+        self.state = value
+
+        shadow = 0
+        if self.state == Button.State.IDLE:
+            shadow = 0
+        elif self.state == Button.State.HOOVER:
+            shadow = 26
+        elif self.state == Button.State.PRESSED:
+            shadow = 33
+        elif self.state == Button.State.RELEASED:
+            shadow = 18
+            self.callback()
+
+        self.sprite.tint = (255-shadow,255-shadow,255-shadow)
+
+
     def update(self) -> None:
         """Update the button pressed status, and call the callback when pressed
         """
-        def overshadow(tint: tuple[int, int, int], increase: int):
-            return (tint[0]-increase,tint[1]-increase,tint[2]-increase)
+        on_hoover = self.sprite.rect.collidepoint(pygame.mouse.get_pos())
+        on_pressed = pygame.mouse.get_pressed()[0]
 
-        mouse_over_btt = self.sprite.rect.collidepoint(pygame.mouse.get_pos())
-        left_mouse_pressed = pygame.mouse.get_pressed()[0]
-
-        if self.pressed: # On realease
-            if not mouse_over_btt:
-                self.pressed = False
-            elif not left_mouse_pressed:
-                self.pressed = False
-                self.callback()
-
-        tint = (255,255,255)
-        if mouse_over_btt:
-            tint = overshadow(tint, 25)
-            if left_mouse_pressed:
-                tint = overshadow(tint, 30)
-                self.pressed = True
-
-        self.sprite.tint = tint
+        if self.state == Button.State.IDLE and on_hoover:
+            self._state = Button.State.HOOVER
+        elif self.state == Button.State.HOOVER:
+            if not on_hoover:
+                self._state = Button.State.IDLE
+            elif on_pressed:
+                self._state = Button.State.PRESSED
+        elif self.state == Button.State.PRESSED:
+            if not on_hoover:
+                self._state = Button.State.IDLE
+            elif not on_pressed:
+                self._state = Button.State.RELEASED
+        elif self.state == Button.State.RELEASED:
+            if on_hoover:
+                self._state = Button.State.PRESSED if on_pressed else Button.State.HOOVER
+            else:
+                self._state = Button.State.IDLE
 
     def render(self, surface: pygame.Surface):
         pass
